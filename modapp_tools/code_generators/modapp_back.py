81bb924d9d7572f17df27ca7e598f8fe24a8d80c
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-07-20 18:38:18 +0200

    Add map to tests

---
+++ modapp_tools/code_generators/modapp_back.py
@@ -0,0 +1,6 @@
+# TODO:
+# 1. Generate model if not exist
+# 2. Generate service per rpc endpoint if not exist
+# 3. Generate persistance per rpc endpoint if not exist
+# 4. Generate service test  if not exist
+# 5. Generate persistance test  if not exist
